8a76493b40c0b7b729a2456f0f689db105ae97e6
sakthi188 <107858315+sakthi188@users.noreply.github.com>
2023-04-09 12:48:57 +0530

    Add files via upload

---
+++ some method.js
@@ -0,0 +1,13 @@
+const items=[
+    {name:'Bike', price:100},
+    {name:'Tv', price:200},
+    {name:'Album',price:10},
+    {name:'Book', price:5},
+    {name:'Phone',price:500},
+    {name:'Computer',price:1000},
+    {name:'Keyboard',price:25}
+]
+const hasInexpensiveItems=items.some((item)=>{
+    return item.price<=100
+})
+console.log(hasInexpensiveItems);
